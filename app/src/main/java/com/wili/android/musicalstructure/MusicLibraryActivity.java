a225d14665bd36ae22e528392100796c29c69cbf
damwilin <damian.wilinski@gmail.com>
2017-05-06 15:13:00 +0200

    Created MusicLibraryActivity. Added design to NowPlaying Activity Implemented OnClickListener to NowPlayingActivity

---
+++ app/src/main/java/com/wili/android/musicalstructure/MusicLibraryActivity.java
@@ -0,0 +1,14 @@
+package com.wili.android.musicalstructure;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MusicLibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_library);
+
+    }
+}
