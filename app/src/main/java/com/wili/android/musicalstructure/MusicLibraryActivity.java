2c5434ec785cc103d7959921a4e3feab21d4a080
damwilin <damian.wilinski@gmail.com>
2017-05-06 15:50:15 +0200

    Added primary design to MusicLibraryActivity Implemented OnClickListener to store_button

--- app/src/main/java/com/wili/android/musicalstructure/MusicLibraryActivity.java
+++ app/src/main/java/com/wili/android/musicalstructure/MusicLibraryActivity.java
@@ -1,7 +1,10 @@
 package com.wili.android.musicalstructure;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MusicLibraryActivity extends AppCompatActivity {
 
@@ -9,6 +12,13 @@ public class MusicLibraryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_library);
+        Button store = (Button) findViewById(R.id.button_store);
+        store.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MusicLibraryActivity.this, StoreActivity.class));
+            }
+        });
 
     }
 }
