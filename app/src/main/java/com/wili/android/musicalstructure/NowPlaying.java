0c169abf8180af50beaf5a839d06b2a357a2b4d2
damwilin <damian.wilinski@gmail.com>
2017-05-11 09:04:31 +0200

    Added basic music functionality

--- app/src/main/java/com/wili/android/musicalstructure/NowPlaying.java
+++ app/src/main/java/com/wili/android/musicalstructure/NowPlaying.java
@@ -1,12 +1,14 @@
 package com.wili.android.musicalstructure;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
 public class NowPlaying extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +28,36 @@ public class NowPlaying extends AppCompatActivity {
                 startActivity(new Intent(NowPlaying.this, SearchActivity.class));
             }
         });
+        //Adding song to player
+        mediaPlayer = MediaPlayer.create(this, R.raw.bensound_cute);
+
+        final ImageView playButton = (ImageView) findViewById(R.id.play_song);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.start();
+                    playButton.setImageResource(R.drawable.ic_pause_black_24dp);
+                } else {
+                    mediaPlayer.pause();
+                    playButton.setImageResource(R.drawable.arrow_black_24dp);
+                }
+            }
+        });
+        ImageView nextSong = (ImageView) findViewById(R.id.next_song);
+        nextSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    private void moveSongToMiddle() {
+        int currPosition = mediaPlayer.getCurrentPosition();
+        int duration = mediaPlayer.getDuration();
+        if ((duration - currPosition) > 15)
+            mediaPlayer.seekTo(currPosition + 15);
     }
 }
